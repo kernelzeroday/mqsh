858b4e94208daf0d1a015074ea78105474d348e6
kod <enktal@gmail.com>
2016-03-23 00:20:06 -0700

    features

--- json/jsh.sh
+++ json/jsh.sh
@@ -2,11 +2,12 @@
 #make a cmd json
 ip=`/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1`
 date=`date +%s`
+uptime=`uptime`
 cmdline="$@"
-cat format.json | sed "s/CHANGEIP/$ip/" | sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"> data.json
+cat format.json | sed "s/CHANGEIP/$ip/" | sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEUPTIME/$uptime/"> data.json
 #cat /dev/stdin>> data.json
 sh -c "$@" >> data.json
-echo \"\}\ >> data.json
+echo \"\} >> data.json
 cat data.json | sed ':a;N;$!ba;s/output\"\ :\ \"\n/output\"\ :\ \"/' 
 >data.json
 
