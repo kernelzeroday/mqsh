cae2ebb69fb8d92de5346a31e79926aee2b000db
kod <enktal@gmail.com>
2016-03-23 01:20:08 -0700

    cleanup

--- json/jsh.sh
+++ json/jsh.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #make a cmd json
 ip=`/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1`
-date=`date +%s`
+unixtime=`date +%s`
+date=`date`
 uptime=`uptime | sed s/\,//`
 ports=`ss -tlp | grep -v Peer |grep 127| tr -s ' ' | cut -f4,6 -d' ' | tr '\n' '!'`
 id=`id`
 version=`cat /proc/version`
 memstat=`cat /proc/meminfo | head -n 3 | tr -s '\n' ' '`
 cmdline="$@"
-cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
+cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEUNIXTIME/$unixtime/"|sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
 #cat /dev/stdin>> data.json
 sh -c "$@" >> data.json
 echo \"\} >> data.json
