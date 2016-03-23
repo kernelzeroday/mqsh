767d3b90b1ecc843e40ce1d35c40eccf53f24559
kod <enktal@gmail.com>
2016-03-22 21:14:11 -0700

    fix

--- json/jsh.sh
+++ json/jsh.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #make a cmd json
 ip=`/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1`
-uname=`uname`
-cat format.json | sed "s/CHANGEIP/$ip/" | sed "s/CHANGEUNAME/$uname/" > data.json
+date=`date +%s`
+cat format.json | sed "s/CHANGEIP/$ip/" | sed "s/CHANGEDATE/$date/" > data.json
 #cat /dev/stdin>> data.json
-sh -c $1 >> data.json
+sh -c "$@" >> data.json
 echo \"\}\ >> data.json
 cat data.json | sed ':a;N;$!ba;s/output\"\ :\ \"\n/output\"\ :\ \"/' 
 >data.json
