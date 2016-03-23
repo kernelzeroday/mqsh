aea963170495abb4dfb523edae2be2b339b70b2e
kod <enktal@gmail.com>
2016-03-22 21:03:35 -0700

    json shit

--- json/jsh.sh
+++ json/jsh.sh
@@ -3,7 +3,8 @@
 ip=`/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1`
 uname=`uname`
 cat format.json | sed "s/CHANGEIP/$ip/" | sed "s/CHANGEUNAME/$uname/" > data.json
-cat /dev/stdin>> data.json
+#cat /dev/stdin>> data.json
+sh -c $1 >> data.json
 echo \"\}\ >> data.json
 cat data.json | sed ':a;N;$!ba;s/output\"\ :\ \"\n/output\"\ :\ \"/' 
 >data.json
