0c54bdf3fd3b70a653e25cdd2a1752c9c56d0370
kod <enktal@gmail.com>
2016-03-23 01:41:08 -0700

    fixes

--- json/jsh.sh
+++ json/jsh.sh
@@ -5,11 +5,13 @@ unixtime=`date +%s`
 date=`date`
 uptime=`uptime | sed s/\,//`
 ports=`ss -tlp | grep -v Peer |grep 127| tr -s ' ' | cut -f4,6 -d' ' | tr '\n' '!'`
+kernelcmdline=`cat /proc/cmdline|sed 's/\//\./g'| sed 's/\-/\\\-/g'`
 id=`id`
+kcrypto=`cat /proc/crypto | grep name | cut -d':' -f2 | uniq | tr -s '\n' ' '`
 version=`cat /proc/version`
 memstat=`cat /proc/meminfo | head -n 3 | tr -s '\n' ' '`
 cmdline="$@"
-cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEUNIXTIME/$unixtime/"|sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
+cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEKCRYPTO/$kcrypto/"|sed "s/CHANGEKERNELCMDLINE/$kernelcmdline/"|sed "s/CHANGEUNIXTIME/$unixtime/"|sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
 #cat /dev/stdin>> data.json
 sh -c "$@" >> data.json
 echo \"\} >> data.json
