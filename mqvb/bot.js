9fdaeb9943b790464b8d52079309943195eb65e8
kod <enktal@gmail.com>
2016-03-22 19:28:06 -0700

    initial commit

---
+++ mqvb/bot.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('ddos.db');
+
+var irc = require('irc');
+
+var bot = new irc.Client('localhost', 'ddosbot', {
+    debug: true,
+    channels: ['#ddos']
+});
+
+bot.addListener('error', function(message) {
+    console.error('ERROR: %s: %s', message.command, message.args.join(' '));
+});
+
+bot.addListener('message#blah', function(from, message) {
+    console.log('<%s> %s', from, message);
+});
+
+bot.addListener('message', function(from, to, message) {
+    console.log('%s => %s: %s', from, to, message);
+
+    if (to.match(/^[#&]/)) {
+        // channel message
+        if (message.match(/hello/i)) {
+            bot.say(to, 'Hello there ' + from);
+        }
+//        if (message.match(/dance/)) {
+ //           setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D\\-<\u0001'); }, 1000);
+  //          setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 2000);
+   //         setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D/-<\u0001');  }, 3000);
+    //        setTimeout(function() { bot.say(to, '\u0001ACTION dances: :D|-<\u0001');  }, 4000);
+     //   }
+    }
+    else {
+        // private message
+        console.log('private message');
+    }
+});
+bot.addListener('pm', function(nick, message) {
+    console.log('Got private message from %s: %s', nick, message);
+});
+bot.addListener('join', function(channel, who) {
+    console.log('%s has joined %s', who, channel);
+});
+bot.addListener('part', function(channel, who, reason) {
+    console.log('%s has left %s: %s', who, channel, reason);
+});
+bot.addListener('kick', function(channel, who, by, reason) {
+    console.log('%s was kicked from %s by %s: %s', who, channel, by, reason);
+});
+
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
