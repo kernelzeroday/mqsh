7da408e7783299354eb9fe272bed085e9841ee27
kod <enktal@gmail.com>
2016-03-23 00:56:20 -0700

    fix

--- json/jsh.sh
+++ json/jsh.sh
@@ -3,10 +3,12 @@
 ip=`/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1`
 date=`date +%s`
 uptime=`uptime | sed s/\,//`
+ports=`ss -tlp | grep -v Peer |grep 127| tr -s ' ' | cut -f4,6 -d' ' | tr '\n' '!'`
+id=`id`
 version=`cat /proc/version`
 memstat=`cat /proc/meminfo | head -n 3 | tr -s '\n' ' '`
 cmdline="$@"
-cat format.json | sed "s/CHANGEIP/$ip/" | sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
+cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
 #cat /dev/stdin>> data.json
 sh -c "$@" >> data.json
 echo \"\} >> data.json
