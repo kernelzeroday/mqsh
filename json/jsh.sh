1317e13759885caca3ee83d0f87da0736d5a61fc
kod <enktal@gmail.com>
2016-03-23 18:32:35 -0700

    update

--- json/jsh.sh
+++ json/jsh.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #make a cmd json
+
 ip=`/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1`
 unixtime=`date +%s`
 date=`date`
@@ -11,11 +12,33 @@ kcrypto=`cat /proc/crypto | grep name | cut -d':' -f2 | uniq | tr -s '\n' ' '`
 version=`cat /proc/version`
 memstat=`cat /proc/meminfo | head -n 3 | tr -s '\n' ' '`
 cmdline="$@"
-cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEKCRYPTO/$kcrypto/"|sed "s/CHANGEKERNELCMDLINE/$kernelcmdline/"|sed "s/CHANGEUNIXTIME/$unixtime/"|sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
-#cat /dev/stdin>> data.json
-sh -c "$@" >> data.json
-echo \"\} >> data.json
-cat data.json | sed ':a;N;$!ba;s/output\"\ :\ \"\n/output\"\ :\ \"/' 
->data.json
+output=$($cmdline)
+
+#old way totally broken
+#cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEKCRYPTO/$kcrypto/"|sed "s/CHANGEKERNELCMDLINE/$kernelcmdline/"|sed "s/CHANGEUNIXTIME/$unixtime/"|sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
+
+echo -n '{
+"ip" : "'$ip'",
+"unixtime": "'$unixtime'",
+"date": "'$date'",
+"uptime" : "'$uptime'",
+"memstat" : "'$memstat'",
+"openports" : "'$ports'",
+"id" : "'$id'",
+"version" : "'$version'",
+"kernel_cmdline": "'$kernelcmdline'",
+"kernel_crypto": "'$kcrypto'",
+"cmdline" : "'$cmdline'",
+"output" : "'$output'"
+"}'
+
+#fuck this fix it
+#sh -c "$@" >> data.json
+#echo \"\} >> data.json
+
+#newline remover dont rly need it fuck it
+#cat data.json |sed 's/\//\./g'|sed ':a;N;$!ba;s/output\"\ :\ \"\n/output\"\ :\ \"/' 
+
+#>data.json
 
 
