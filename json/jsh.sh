a4345b755d2f9567ae5c5dbc19ed66b4e2b5bcb7
kod <enktal@gmail.com>
2016-03-24 13:06:57 -0700

    jshon

--- json/jsh.sh
+++ json/jsh.sh
@@ -1,44 +1,41 @@
 #!/bin/sh
 #make a cmd json
-
-ip=`/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1`
-unixtime=`date +%s`
-date=`date`
-uptime=`uptime | sed s/\,//`
-ports=`ss -tlp | grep -v Peer |grep 127| tr -s ' ' | cut -f4,6 -d' ' | tr '\n' '!'`
-kernelcmdline=`cat /proc/cmdline|sed 's/\//\./g'| sed 's/\-/\\\-/g'`
-id=`id`
-kcrypto=`cat /proc/crypto | grep name | cut -d':' -f2 | uniq | tr -s '\n' ' '`
-version=`cat /proc/version`
-memstat=`cat /proc/meminfo | head -n 3 | tr -s '\n' ' '`
+bbj=`busybox jshon -s `
+ip=$bbj''"$(/sbin/ifconfig lo | grep Mask | cut -d ':' -f2 | cut -d " " -f1)"''
+unixtime=$bbj"$(date +%s)"
+date=$bbj"$(date)"
+uptime=$bbj"$(uptime | sed s/\,//)"
+kernelcmdline=$bbj"$(cat /proc/cmdline)"
+id=$bbj"$(id)"
+kcrypto=$bbj"$(cat /proc/crypto | grep name | cut -d':' -f2 | uniq | tr -s '\n' ' '|sed s/\,//)"
+version=$bbj"$(cat /proc/version)"
+memstat=$bbj"$(cat /proc/meminfo | head -n 3 | tr -s '\n' ' ')"
+cwd=$bbj"$(pwd)"
+shell=$bbj"$(echo $SHELL)"
 cmdline="$@"
-output=$($cmdline)
+output=$bbj"$($cmdline)"
+getstty=$bbj"$(stty)"
+term=$bbj"$(echo $TERM)"
+cpuname=$bbj"$(cat /proc/cpuinfo | grep name)"
 
-#old way totally broken
-#cat format.json | sed "s/CHANGEIP/$ip/" |sed "s/CHANGEKCRYPTO/$kcrypto/"|sed "s/CHANGEKERNELCMDLINE/$kernelcmdline/"|sed "s/CHANGEUNIXTIME/$unixtime/"|sed "s/CHANGEID/$id/"| sed "s/CHANGEPORTS/$ports/"|sed "s/CHANGEDATE/$date/" | sed "s/CHANGECMDLINE/$cmdline/"|sed "s/CHANGEMEM/$memstat/"|sed "s/CHANGEUPTIME/$uptime/"|sed "s/CHANGEVERSION/$version/"> data.json
-
-echo -n '{
+echo '{
 "ip" : "'$ip'",
 "unixtime": "'$unixtime'",
 "date": "'$date'",
 "uptime" : "'$uptime'",
+"cpuname": "'$cpuname'",
 "memstat" : "'$memstat'",
-"openports" : "'$ports'",
 "id" : "'$id'",
 "version" : "'$version'",
 "kernel_cmdline": "'$kernelcmdline'",
 "kernel_crypto": "'$kcrypto'",
+"shell": "'$shell'",
+"term": "'$term'",
+"stty": "'$getstty'",
+"cwd": "'$cwd'",
 "cmdline" : "'$cmdline'",
 "output" : "'$output'"
 "}'
 
-#fuck this fix it
-#sh -c "$@" >> data.json
-#echo \"\} >> data.json
-
-#newline remover dont rly need it fuck it
-#cat data.json |sed 's/\//\./g'|sed ':a;N;$!ba;s/output\"\ :\ \"\n/output\"\ :\ \"/' 
-
-#>data.json
 
 
